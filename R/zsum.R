c723362c1db9347a2581fb2347221624785665b9
mz74 <martin.zehetmayer@gmx.net>
2021-01-10 16:24:28 +0100

    2020-01-10 Add function zsum

---
+++ R/zsum.R
@@ -0,0 +1,15 @@
+#' Sum two numerics
+#'
+#' Two numerical numbers are summend
+#'
+#' @param a numeric
+#' @param b numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' zsum(2,5)
+zsum = function(a, b){
+  return (a+b)
+}
